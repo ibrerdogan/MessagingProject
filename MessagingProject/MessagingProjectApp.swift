2662c51483f9b7d74a459d71b56eac7feafe6817
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2022-04-25 13:32:47 +0300

    Initial Commit

---
+++ MessagingProject/MessagingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingProjectApp.swift
+//  MessagingProject
+//
+//  Created by İbrahim Erdogan on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MessagingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
